42d8c69c669c7dd4af3f0ac830866c6c98f5f8ef
Tinsae Tadesse <tinsaetadesse2015@gmail.com>
2025-05-09 10:50:02 +0300

    Updated Provisioning Script

--- vagrant-scripts/provisioning.sh
+++ vagrant-scripts/provisioning.sh
@@ -1,13 +1,45 @@
 #!/bin/bash
 
 # Update the OS
-echo "Updating OS packages..."
+echo "---------> Updating OS packages..."
 sudo apt-get update -y
 
 # Install Git
-echo "Installing git..."
+echo "---------> Installing Git..."
 sudo apt-get install git -y
 
 # Install Apache
-echo "Installing apache2..."
+echo "---------> Installing Apache2..."
 sudo apt-get install apache2 -y
+
+# Stop Apache2 Service
+echo "---------> Stoping Apache2 Service..."
+sudo systemctl stop apache2.service
+
+# Download Files from Remote Repository
+echo "---------> Cloning Website..."
+git clone https://github.com/Tinsae-Tadesse/devops-vagrant.git
+sudo mv ./devops-vagrant my-site.com
+sudo mv ./my-site.com /var/www/
+
+# Configure Apache2
+sudo echo "---------> Configuring Apache2..."
+sudo echo """
+<VirtualHost *:80>
+    ServerName my-site.com
+    DocumentRoot /var/www/my-site.com
+    <Directory /var/www/my-site.com>
+        Options Indexes FollowSymLinks
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+""" > /etc/apache2/sites-available/my-site.com.conf
+sudo chown -R $USER:$USER /var/www/my-site.com
+sudo chmod -R 755 /var/www/my-site.com
+
+# Start Apache2 Service
+echo "---------> Starting Apache2 Service..."
+sudo systemctl start apache2.service
+sudo a2dissite 000-default.conf
+sudo a2ensite my-site.com.conf
