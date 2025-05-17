c67f49b3dca50266ad400faef200e7d8762b5c0e
Tinsae-Tadesse <tinsaetadesse2015@gmail.com>
2025-05-17 09:54:14 +0300

    Updated provisioning scripts for the web and database servers

--- vagrant-scripts/db-provisioning.sh
+++ vagrant-scripts/db-provisioning.sh
@@ -5,15 +5,18 @@ DB_VM_IP=$1
 # Configure upstream DNS server
 echo "==> Fixing DNS..."
 echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
+echo "==> Finished fixing DNS."
 
 # Update the OS
 echo "==> Updating OS packages..."
 sudo apt-get update -y
 sudo apt-get install build-essential dkms linux-headers-$(uname -r) -y
+echo "==> Finished updating OS packages."
 
 # Install MySQL
 echo "==> Installing MySQL..."
 sudo apt-get install mysql-server -y
+echo "==> Finished installing MySQL."
 
 # Define environment variables
 ROOT_USER="root"
@@ -24,6 +27,8 @@ DB_USER_PASSWORD="mypassword"
 
 # Configure MySQL
 echo "==> Configuring MySQL..."
+sudo sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
+
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $ROOT_PASSWORD"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $ROOT_PASSWORD"
 
@@ -31,9 +36,10 @@ mysql -u$ROOT_USER -p$ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $DB_NAME C
 mysql -u$ROOT_USER -p$ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$DB_USER'@'$WEB_VM_IP' IDENTIFIED BY '$DB_USER_PASSWORD';"
 mysql -u$ROOT_USER -p$ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'$WEB_VM_IP';"
 mysql -u$ROOT_USER -p$ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+echo "==> Finished configuring MySQL."
 
 # Configure MySQL
-echo "==> Creating database table..."
+echo "==> Creating database tables..."
 mysql -u$ROOT_USER -p$ROOT_PASSWORD $DB_NAME <<EOF
 CREATE TABLE IF NOT EXISTS contact_messages (
   id INT AUTO_INCREMENT PRIMARY KEY,
@@ -44,3 +50,4 @@ CREATE TABLE IF NOT EXISTS contact_messages (
   submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 EOF
+echo "==> Finished creating database tables."
--- vagrant-scripts/web-provisioning.sh
+++ vagrant-scripts/web-provisioning.sh
@@ -8,15 +8,18 @@ DB_NAME=$4
 # Configure upstream DNS server
 echo "==> Fixing DNS..."
 echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
+echo "==> Finished fixing DNS."
 
 # Update the OS
 echo "==> Updating OS packages..."
 sudo apt-get update -y
 sudo apt-get install build-essential dkms linux-headers-$(uname -r) -y
+echo "==> Finished updating OS packages."
 
 # Install Apache2 and Git
 echo "==> Installing Apache2, php, and Git..."
 sudo apt-get install apache2 php libapache2-mod-php php-mysql git -y
+echo "==> Finished installing Apache2, php, and Git."
 
 # Define target directory and GitHub repo
 SITE_NAME="web.example.local"
@@ -28,17 +31,20 @@ REPO_URL="https://github.com/Tinsae-Tadesse/devops-vagrant.git"
 # Stop Apache2 Service
 echo "==> Stoping Apache2 Service..."
 sudo systemctl stop apache2.service
+echo "==> Finished stoping Apache2 service."
 
 # Cleaning up Files and Directories
 echo "==> Cleaning up files and directories..."
 sudo rm -fr $SITE_NAME
 sudo rm -f "$SITE_CONF_DIR/$SITE_NAME.conf"
 sudo rm -fr $WEB_ROOT
+echo "==> Finished cleaning up files and directories."
 
 # Download Files from Remote Repository
-echo "==> Cloning Website..."
+echo "==> Cloning website..."
 git clone $REPO_URL $SITE_NAME
 sudo mv $SITE_NAME /var/www/
+echo "==> Finished cloning wbsite."
 
 # Configure Apache2
 echo "==> Configuring Apache2..."
@@ -64,8 +70,10 @@ echo """
 """ | sudo tee "$SITE_CONF_DIR/$SITE_NAME.conf" > /dev/null
 sudo chown -R $USER:$USER $WEB_ROOT
 sudo chmod -R 755 $WEB_ROOT
+echo "==> Finished configuring Apache2."
 
 # Configuring php
+echo "==> Configuring PHP..."
 echo """
 <?php
 define('DB_HOST', '${DB_HOST}');
@@ -75,9 +83,11 @@ define('DB_NAME', '${DB_NAME}');
 ?>
 """ | sudo tee "$PHP_ROOT/config.php.template" > /dev/null
 sudo mv "$PHP_ROOT/config.php.template" "$PHP_ROOT/config.php"
+echo "==> Finished configuring PHP."
 
 # Start Apache2 Service
 echo "==> Starting Apache2 Service..."
 sudo systemctl start apache2.service
 sudo a2dissite 000-default.conf
 sudo a2ensite "$SITE_NAME.conf"
+echo "==> Finished starting Apache2 service."
